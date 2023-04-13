b026be4ccb6560a453ec6753bb7f786f3587e186
Franco Loto <franco.loto1995@gmail.com>
2023-04-12 21:18:45 -0300

    django authentication

--- tienda-golosinas/settings.py
+++ tienda-golosinas/settings.py
@@ -128,3 +128,19 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_TIMEOUT = 60
+EMAIL_HOST_USER = 'franco.loto1995@gmail.com'
+EMAIL_HOST_PASSWORD = 'nvcmnaiezzjhgzez'
+EMAIL_USE_TLS = True
